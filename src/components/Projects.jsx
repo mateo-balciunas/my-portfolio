0d0885114deb919bd7d4af5622ae5daf81d9d9e9
mateo-balciunas <mateo.balciunas@icloud.com>
2026-05-17 13:41:35 +0200

    feat: added project component and styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,11 +2,38 @@ import React from "react";
 
 function Projects() {
 
+    const projectsContent = [
+        {
+            title: "Portfolio",
+            description: "Portfolio de desarrollador web.",
+            tags: ["React", "JavaScript", "CSS", "HTML"],
+            github: "https://github.com/mateo-balciunas/my-portfolio.git",
+        }
+    ]
     return (
-
-        <div>
-            <h2>PROJECTS COMPONENT</h2>
-        </div>
+        <section id="projects" className="projects">
+            <h2>Projects</h2>
+            <div className="projects-grid">
+                {projectsContent.map((project, index) => (
+                    <div key={index} className="project-card">
+                        <div className="project-content">
+                        <h3>{project.title}</h3>
+                        <p>{project.description}</p>
+                        <div className="project-tags">
+                            {project.tags.map((tag, i) => (
+                                <span key={i} className="tag">
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                            <div className="project-links">
+                                <a href={project.github} target="_blank" rel="noreferrer" className="btn-secondary">GitHub</a>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
     );
 }
 
